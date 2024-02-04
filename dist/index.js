978d11ff323ebbfdc0bd524bfd5c0d870f120147
neo062 <navinkumar241999@gmail.com>
2024-02-04 20:52:26 +0530

    crud appplication using prisma

---
+++ dist/index.js
@@ -0,0 +1,68 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// require('dotenv').config()
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function insertUser(username, password, firstName, lastName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.create({
+            data: {
+                email: username,
+                password,
+                firstName,
+                lastName
+            }
+        });
+        console.log('res', res);
+    });
+}
+function updateUser(username, { firstName, lastName }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.update({
+            where: { email: username },
+            data: {
+                firstName,
+                lastName
+            }
+        });
+        console.log('res', res);
+    });
+}
+function getUser(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.findUnique({
+            where: {
+                email: username
+            }
+        });
+        console.log('res>>', res);
+    });
+}
+function getAllUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.findMany();
+        console.log('data>>', res);
+    });
+}
+function deleteUser(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.findFirst({
+            where: { email: username }
+        });
+        console.log('res', res);
+    });
+}
+// insertUser("shravan@gmail.com", "pass@123", "Shravan", "Kumar")
+// updateUser("navinkumar241999@gmail.com", { firstName: "Coder", lastName: "Navin" })
+// getUser("navinkumar241999@gmail.com")
+// getAllUser()
+deleteUser('navinkumar241999@gmail.com');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-require('dotenv').config()
+// require('dotenv').config()
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+    const res = await prisma.user.create({
+        data: {
+            email: username,
+            password,
+            firstName,
+            lastName
+        }
+    })
+    console.log('res', res);
+}
+
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+    const res = await prisma.user.update({
+        where: { email: username },
+        data: {
+            firstName,
+            lastName
+        }
+    })
+    console.log('res', res)
+}
+
+async function getUser(username: string) {
+    const res = await prisma.user.findUnique({
+        where: {
+            email: username
+        }
+    })
+    console.log('res>>', res)
+
+}
+async function getAllUser() {
+    const res = await prisma.user.findMany()
+    console.log('data>>', res);
+}
+
+async function deleteUser(username: string) {
+    const res = await prisma.user.findFirst({
+        where: { email: username }
+    })
+    console.log('res', res);
+}
+// insertUser("shravan@gmail.com", "pass@123", "Shravan", "Kumar")
+// updateUser("navinkumar241999@gmail.com", { firstName: "Coder", lastName: "Navin" })
+// getUser("navinkumar241999@gmail.com")
+// getAllUser()
+// deleteUser('navinkumar241999@gmail.com')
